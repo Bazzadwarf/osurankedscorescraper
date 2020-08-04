fd9ddd929b758bfe5df0138f04102a7eae40131c
Louis Hobbs <bazzadobs@live.co.uk>
2020-08-04 21:53:52 +0100

    Finalise basic scrapper

--- osurankedsscorescraper/Player.cs
+++ osurankedsscorescraper/Player.cs
@@ -1,4 +1,5 @@
-﻿using HtmlAgilityPack;
+﻿using System;
+using HtmlAgilityPack;
 
 namespace osurankedsscorescraper
 {
@@ -9,60 +10,106 @@ namespace osurankedsscorescraper
         public string Username { get; set; }
         public double Accuracy { get; set; }
         public int PlayCount { get; set; }
-        public int TotalScore { get; set; }
-        public int RankedScore { get; set; }
+        public long TotalScore { get; set; }
+        public long RankedScore { get; set; }
         public int SsRankCount { get; set; }
         public int SRankCount { get; set; }
         public int ARankCount { get; set; }
 
-        public Player(HtmlNode inputHtml)
+        public Player()
         {
-            
+            Rank = int.MaxValue;
+            Country = string.Empty;
+            Username = string.Empty;
+            Accuracy = double.MinValue;
+            PlayCount = int.MinValue;
+            TotalScore = long.MinValue;
+            RankedScore = long.MinValue;
+            SsRankCount = int.MinValue;
+            SRankCount = int.MinValue;
+            ARankCount = int.MinValue;
+        }
+        
+        public Player(HtmlNode inputNode)
+        {
+            ParseRank(inputNode);
+            ParseCountry(inputNode);
+            ParseUsername(inputNode);
+            ParseAccuracy(inputNode);
+            ParsePlayCount(inputNode);
+            ParseTotalScore(inputNode);
+            ParseRankedScore(inputNode);
+            ParseSsRankCount(inputNode);
+            ParseSRankCount(inputNode);
+            ParseARankCount(inputNode);
+
+            Console.WriteLine(Rank + " " + Country + " " + Username + " " + Accuracy + " " + PlayCount + " " + TotalScore + " " + RankedScore + " " + SsRankCount + " " + SRankCount + " " + ARankCount);
         }
 
         private void ParseRank(HtmlNode inputNode)
         {
-            
+            Rank = int.Parse(inputNode.ChildNodes[1].InnerText.Trim().Remove(0, 1));
         }
 
+        private void ParseCountry(HtmlNode inputNode)
+        {
+            var getHrefValue = inputNode.ChildNodes[3].InnerHtml.Split('"');
+
+            Country = getHrefValue[7];
+        }
+        
         private void ParseUsername(HtmlNode inputNode)
         {
-            
+            Username = inputNode.ChildNodes[3].InnerText.Trim();
         }
 
         private void ParseAccuracy(HtmlNode inputNode)
         {
-            
+            var acc = inputNode.ChildNodes[5].InnerText.Trim();
+
+            Accuracy = double.Parse(acc.Remove(acc.Length - 1, 1));
         }
 
         private void ParsePlayCount(HtmlNode inputNode)
         {
-            
+            PlayCount = int.Parse(inputNode.ChildNodes[7].InnerText.Trim().Replace(",", ""));
         }
-        
+
         private void ParseTotalScore(HtmlNode inputNode)
         {
-            
+            var innerHtml = inputNode.ChildNodes[9].InnerHtml;
+
+            var i = innerHtml.IndexOf("'", StringComparison.Ordinal);
+            var substring = innerHtml.Substring(i + 1, i);
+            substring = substring.Substring(0, substring.IndexOf("'", StringComparison.Ordinal));
+            substring = substring.Replace(",", "");
+            TotalScore = long.Parse(substring);
         }
-        
+
         private void ParseRankedScore(HtmlNode inputNode)
         {
-            
+            var innerHtml = inputNode.ChildNodes[11].InnerHtml;
+
+            var i = innerHtml.IndexOf("'", StringComparison.Ordinal);
+            var substring = innerHtml.Substring(i + 1, i);
+            substring = substring.Substring(0, substring.IndexOf("'", StringComparison.Ordinal));
+            substring = substring.Replace(",", "");
+            RankedScore = long.Parse(substring);
         }
-        
+
         private void ParseSsRankCount(HtmlNode inputNode)
         {
-            
+            SsRankCount = int.Parse(inputNode.ChildNodes[13].InnerText.Trim().Replace(",", ""));
         }
-        
+
         private void ParseSRankCount(HtmlNode inputNode)
         {
-            
+            SRankCount = int.Parse(inputNode.ChildNodes[15].InnerText.Trim().Replace(",", ""));
         }
-        
+
         private void ParseARankCount(HtmlNode inputNode)
         {
-            
+            ARankCount = int.Parse(inputNode.ChildNodes[17].InnerText.Trim().Replace(",", ""));
         }
     }
 }
--- osurankedsscorescraper/Program.cs
+++ osurankedsscorescraper/Program.cs
@@ -1,9 +1,36 @@
-﻿namespace osurankedsscorescraper
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using HtmlAgilityPack;
+using CsvHelper;
+
+namespace osurankedsscorescraper
 {
     internal class Program
     {
         public static void Main(string[] args)
         {
+            var web = new HtmlWeb();
+            const string baseUrl = "https://osu.ppy.sh/rankings/osu/score?page=";
+            
+            TextWriter textWriter = new StreamWriter("test.csv");
+            
+            var csv = new CsvWriter(textWriter, CultureInfo.CurrentCulture);
+            
+            for (var pageNumber = 1; pageNumber <= 200; pageNumber++)
+            {
+                var doc = web.Load(baseUrl + pageNumber);
+            
+                var tableBody = doc.DocumentNode.SelectNodes("//table//tbody//tr");
+                
+                csv.WriteRecords(tableBody.Select(tableRow => new Player(tableRow)).ToList());
+                csv.Flush();
+                System.Threading.Thread.Sleep(1000);
+            }
+            textWriter.Close();
         }
     }
 }
